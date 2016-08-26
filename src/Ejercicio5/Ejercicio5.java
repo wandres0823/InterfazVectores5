f76fa45099ed8a5c8dbb96f72622deab18f9c3c5
“william” <william200928@gmail.com>
2016-08-26 06:47:34 +0200

    este es el ejercicio 5

--- src/Ejercicio5/Ejercicio5.java
+++ src/Ejercicio5/Ejercicio5.java
@@ -172,7 +172,7 @@ public class Ejercicio5 extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 255, 0));
         jLabel1.setText("SUMATORIA Y PRODUCTORIA");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 20, -1, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
